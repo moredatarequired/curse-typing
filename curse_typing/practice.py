bb3ee9f1934c23dc869a8a4b67b21a578784ca88
Hugh Wimberly <hugh.wimberly@gmail.com>
2020-06-16 18:12:51 -0700

    Correctly handle non-typeable characters

--- curse_typing/practice.py
+++ curse_typing/practice.py
@@ -1,43 +1,52 @@
-from markov_text import english_nonsense
+from string import printable
 import curses
 from curses import wrapper
 import time
 
+from reader import sentence_generator
+
 
 def practice(stdscr):
     stdscr.clear()
-    while True:
-        practice_sentence(stdscr)
+    for sentence in sentence_generator():
+        practice_sentence(stdscr, sentence)
 
 
-def practice_sentence(stdscr):
-    sentence = english_nonsense()
+def practice_sentence(stdscr, sentence):
+    sentence = "".join(c for c in sentence.strip() if c in printable)
+    y, x = curses.getsyx()
     stdscr.addstr(sentence + "\n")
+    stdscr.move(y, x)
 
     done = False
-    start = time.perf_counter()
+    start = None
     i = 0
     keystrokes = []
 
     while not done:
         c = stdscr.getkey()
+        if start is None:
+            start = time.perf_counter()
         t = time.perf_counter() - start
         keystrokes.append((c, t))
 
-        stdscr.addch(c)
         if c == sentence[i]:
             i += 1
+            stdscr.move(y, x + i)
             if i >= len(sentence):
                 done = True
-            y, x = curses.getsyx()
-            stdscr.move(y, x + 1)
         stdscr.refresh()
+
+    avg = keystrokes[-1][1] / len(sentence)
+    mistakes = len(keystrokes) - len(sentence)
+    stdscr.move(y, 80)
+    stdscr.addstr(f"  # wrong: {mistakes}; speed: {avg}\n")
 
     y, x = curses.getsyx()
     stdscr.move(y + 1, 0)
     stdscr.refresh()
 
-    return sentence, keystrokes
+    return keystrokes
 
 
 if __name__ == "__main__":
--- curse_typing/reader.py
+++ curse_typing/reader.py
@@ -3,26 +3,38 @@ from pathlib import Path
 
 SOURCE = 'untyped.txt'
 
-with open(SOURCE) as infile:
-    book = infile.readlines()
 
 def line_to_sentences(line):
     words = line.split()
-    next_part = []
+    next_part = ''
     while words:
-        if len(words[0]) + sum(len(s) for s in next_part) < 80:
-            next_part.append(words[0])
-            words = words[1:]
+        if len(words[0]) + len(next_part) >= 80:
+            yield next_part
+            next_part = ''
         else:
-            yield ' '.join(next_part)
-            next_part = []
+            next_part = f"{next_part} {words[0]}"
+            words = words[1:]
     if next_part:
-        yield ' '.join(next_part)
+        yield next_part
 
-sentences = []
-for line in book:
-    sentences.extend(line_to_sentences(line.strip()))
+def create_sentences():
+    with open(SOURCE) as infile:
+        book = infile.readlines()
 
-with open('sentences-cory.txt', 'w') as outfile:
-    for sentence in sentences:
-        outfile.write(f'{sentence}\n')
+    sentences = []
+    for line in book:
+        sentences.extend(line_to_sentences(line.strip()))
+
+    with open('sentences-cory.txt', 'w') as outfile:
+        for sentence in sentences:
+            outfile.write(f'{sentence}\n')
+
+
+def sentence_generator():
+    with open('sentences-cory.txt') as infile:
+        for sentence in infile:
+            yield sentence
+
+
+if __name__ == "__main__":
+    create_sentences()
